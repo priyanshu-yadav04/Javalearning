729f45d08e32d8761a159f7aa70b75f315eb15af
priyanshu-yadav04 <pu553525@gmail.com>
2026-03-04 21:31:07 +0530

    3

--- Function/FunctionPrime.java
+++ Function/FunctionPrime.java
@@ -1,23 +1,35 @@
 import java.util.Scanner;
 public class FunctionPrime{
     public static boolean isPrime(int n){
-        boolean isPrime = true;
-        for(int i =2 ; i <= n-1;i++){
+        if(n==2){
+            return true;
+        }
+        
+        for(int i =2 ; i <= Math.sqrt(n);i++){
             if(n%i ==0){
-                isPrime = false;
-                break;
-
-
+                return  false;
+            
             }
 
         }
-        return isPrime;
+        return true;
+    }
+    public static void PrimeRange(int n) {
+        for (int i = 2; i <=n; i++) {
+            if (isPrime(i)){
+            System.out.println(i+ " ");}
+            
+        }
+        System.out.println();
+       
+        
     }
     public static void main(String[] args) {
         Scanner sc =new Scanner(System.in);
         System.out.println("enter a number is prime or not:  ");
         int n = sc.nextInt();
-        System.err.println(isPrime(n));
+        System.out.println(isPrime(n));
+        PrimeRange(n);
         
     }
     
